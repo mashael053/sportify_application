19be072f6150f30c96a5732a17f76f4bd87cad68
0538969859Mm <mshalalwajy08@gmail.com>
2024-04-02 23:18:34 +0300

    Create Drawer

---
+++ lib/screens/players_screen.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter/material.dart';
+
+// class players_screen extends StatelessWidget {
+//   const players_screen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(backgroundColor: Color.fromARGB(100, 161, 195, 152)),
+//     );
+//   }
+// }
---
+++ lib/section2/DrawerScreen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class DrawerScreen extends StatelessWidget {
+  const DrawerScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sportify",
+          style: TextStyle(color: Colors.white , fontWeight: FontWeight.w400), // Set text color to white
+        ),
+        backgroundColor: Color(0xFFA1C398), // Set background color to red
+        centerTitle: true, // Center the title
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.white, // Set the background color to white
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(13),
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        UserAccountsDrawerHeader(
+                          accountName: const Text(
+                            'UserName',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black, // Set the text color to white
+                            ),
+                          ),
+                          accountEmail: null,
+                          currentAccountPicture: CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assest/icon-profile.jpg'), // Fix typo in asset path
+                          ),
+                          decoration: BoxDecoration(
+                            // color: const Color(0xFFA1C398),
+                          ),
+                        ),
+                        ListTile(
+                          title: const Text('Mashael Ali'),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFA1C398),
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 0, horizontal: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 30,
+                    child: const Text(
+                      'Log Out',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
