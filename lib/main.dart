97072ba9331c69a5b099d9966abad1a9db243964
0538969859Mm <mshalalwajy08@gmail.com>
2024-04-03 22:41:47 +0300

    Done Code for Countries

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:sportify_application/section2/homePage.dart';
+import 'package:sportify_application/screens/countries_screen.dart';
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-
+  const MyApp({super.key});
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sportify Application',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: Homepage(),
+    );
+  }
+}
